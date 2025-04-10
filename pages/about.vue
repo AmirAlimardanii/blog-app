1c1f7b93c1e461be1133c9d948b75295d7340ced
a.alimardani <amiralimardani.dev@gmail.com>
2025-04-10 14:47:39 +0330

    refactor: update app structure with new layout and pages

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is the about page.</p>
+
+    <p>It contains information about the application.</p>
+    <p>It is a simple example of a Nuxt 3 page.</p>
+
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+
+<script setup></script>
