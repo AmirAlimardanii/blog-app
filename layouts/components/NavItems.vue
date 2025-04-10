4e10d133e065210b4aafe864c3b9b9f50a094c7c
a.alimardani <amiralimardani.dev@gmail.com>
2025-04-10 16:27:28 +0330

    feat: add navigation component and update default layout

---
+++ layouts/components/NavItems.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="text-black font-mono">
+    <ul>
+      <li class="mx-2">
+        <nuxt-link class="link" to="/about">About</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link class="link" to="/projects">Projects</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.link {
+  @apply hover:bg-gray-200 p-1 rounded-md;
+}
+
+li {
+  display: inline-block;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,19 @@
 <template>
-  default layout
-  <div>
-    <slot />
+  <div class="container p-2 mx-auto max-w-2xl">
+    <header class="flex justify-between items-center py-1">
+      <div class="text-xl font-semibold">
+        <nuxt-link class="hover:bg-gray-200 p-1 rounded-md" to="/"
+          >Amir Alimardani
+        </nuxt-link>
+      </div>
+      <NavItems />
+    </header>
+    <main class="P-2">
+      <slot />
+    </main>
   </div>
 </template>
+
+<script setup lang="ts">
+import NavItems from "./components/NavItems.vue"
+</script>
